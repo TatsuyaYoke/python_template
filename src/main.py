b05c64639aec6899fe79b5c057be0f876683895c
Yoke <tayok@synspective.com>
2022-06-22 13:09:31 +0900

    modified logger.py and add settings.py for .env

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
+from __future__ import annotations
+
 import sys
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import (
-    Any,
-    Dict,
-    Final,
-    List,
-    Literal,
-    Optional,
-    Tuple,
-    TypedDict,
-    Union,
-    cast,
-)
+from typing import Any, Final, Literal, Optional, TypedDict, Union, cast
 
 import pandas as pd
 import pandera as pa
 from pydantic import BaseModel, Field, error_wrappers
+
+import common.settings
+from common.logger import set_logger
+
+LOGGER_IS_ACTIVE_STREAM = common.settings.logger_is_active_stream
+
+logger = set_logger(__name__, is_active_stream=LOGGER_IS_ACTIVE_STREAM)
 
 if getattr(sys, "frozen", False):
     p_file = Path(sys.executable)
@@ -61,30 +59,31 @@ def advanced_typing_sample() -> None:
     animal2: Animal = {"name": "cat", "age": 20, "height": 60.5, "weight": 25.2}
     # {"name": "cat"} -> NG
     # {"name": "cat", "age": 20, "height": 175.5} -> OK
-    print(type(animal1), type(animal2))
-    # <class 'dict'> <class 'dict'>
+    logger.info(type(animal1))  # <class 'dict'>
+    logger.info(type(animal2))  # <class 'dict'>
 
     # class
     user = User(name="yamada", age=18, height=175.2, weight=75.2)
-    print(f"{user.name} BMI: {user.bmi}")
+    logger.info(f"{user.name} BMI: {user.bmi}")
     # yamada BMI: 24.499072162798946
 
     # cast
     # When type inference does not work and the type is Any for some reason
     any_value: Any = ["a", "b"]
-    string_list = cast(List[str], any_value)
+    string_list = cast("list[str]", any_value)
     number_str = "1"
     number = int(number_str)
     string_int = str(number)
-    print(type(string_list), type(number), type(string_int))
-    # <class 'list'> <class 'int'> <class 'str'>
+    logger.info(type(string_list))  # <class 'list'>
+    logger.info(type(number))  # <class 'int'>
+    logger.info(type(string_int))  # <class 'str'>
 
     # pydantic
     try:
         order = Order.parse_file(p_file / "example_external_file/order.json")
     except error_wrappers.ValidationError:
         order = None
-    print(order)
+    logger.info(order)
     # name='rice' created_at=datetime.datetime(2022, 1, 1, 0, 0) price=200
 
     # pandera
@@ -103,7 +102,7 @@ def advanced_typing_sample() -> None:
         validated_df = schema_df(df)
     except pa.errors.SchemaError:
         validated_df = None
-    print(validated_df)
+    logger.info(validated_df)
     #     name           created_at  price
     # 0   rice  2022-01-01 00:00:00    100
     # 1  ramen  2022-01-02 00:00:00    500
@@ -119,67 +118,70 @@ def basic_typing_sample() -> None:
     item2_1: float = 2.5
     item3_1: str = "string"
     item4_1: bool = True
-    print(type(item1_1), type(item2_1), type(item3_1), type(item4_1))
-    # <class 'int'> <class 'float'> <class 'str'> <class 'bool'>
+    logger.info(type(item1_1))  # <class 'int'>
+    logger.info(type(item2_1))  # <class 'float'>
+    logger.info(type(item3_1))  # <class 'str>
+    logger.info(type(item4_1))  # <class 'bool'>
 
     # You can omit writing type hints because of type inference by editor
     item1_2 = 1
     item2_2 = 2.5
     item3_2 = "string"
     item4_2 = True
-    print(type(item1_2), type(item2_2), type(item3_2), type(item4_2))
-    # <class 'int'> <class 'float'> <class 'str'> <class 'bool'>
+    logger.info(type(item1_2))  # <class 'int'>
+    logger.info(type(item2_2))  # <class 'float'>
+    logger.info(type(item3_2))  # <class 'str'>
+    logger.info(type(item4_2))  # <class 'bool'>
 
     # Optional
     item5: Optional[int] = 1
     item5 = None  # None acceptable
-    print(type(item5))
-    # <class 'NoneType'>
+    logger.info(type(item5))  # <class 'NoneType'>
 
     # Union
     item6: Union[int, None] = 1  # Same as Optional
     item6 = None
     item7: Union[int, str] = "string"
     item7 = 1
-    print(type(item6), type(item7))
-    # <class 'NoneType'> <class 'int'>
+    logger.info(type(item6))  # <class 'NoneType'>
+    logger.info(type(item7))  # <class 'int'>
 
     # Literal
     item8: Literal["a", "b"] = "a"  # item8 = "c" -> NG
     item9: Literal[1, 2] = 2  # item9 = 3 -> NG
-    print(type(item8), type(item9))
-    # <class 'str'> <class 'int'>
+    logger.info(type(item8))  # <class 'str'>
+    logger.info(type(item9))  # <class 'int'>
 
     # Final
     # Cannot change value. Final is used for constant value
     item10: Final[int] = 0  # item10 = 1 -> NG
-    print(type(item10))
-    # <class 'int'>
+    logger.info(type(item10))  # <class 'int'>
 
     # List
-    # List is used in python 3.6 to 3.8 instead of list which is used in python 3.9 or more
-    list1: List[str] = ["a", "b"]  # list1.append(1) -> NG
-    list2: List[int] = [1, 2, 3]  # list2.append("a") -> NG
-    list3: List[int] = []  # Empty acceptable
-    list4: List[Union[int, str, None]] = ["string", 2, None]
-    print(type(list1), type(list2), type(list3), type(list4))
-    # <class 'list'> <class 'list'> <class 'list'> <class 'list'>
+    list1: list[str] = ["a", "b"]  # list1.append(1) -> NG
+    list2: list[int] = [1, 2, 3]  # list2.append("a") -> NG
+    list3: list[int] = []  # Empty acceptable
+    list4: list[Union[int, str, None]] = ["string", 2, None]
+    logger.info(type(list1))  # <class 'list'>
+    logger.info(type(list2))  # <class 'list'>
+    logger.info(type(list3))  # <class 'list'>
+    logger.info(type(list4))  # <class 'list'>
 
     # Tuple
-    # Tuple is used in python 3.6 to 3.8 instead of tuple which is used in python 3.9 or more
-    tuple1: Tuple[int, int] = (1, 2)
-    tuple2: Tuple[int, str, List[int]] = (1, "string", [1, 2, 3])
-    tuple3: Tuple[str, ...] = ("a", "b", "c")  # any length acceptable
-    print(type(tuple1), type(tuple2), type(tuple3))
-    # <class 'tuple'> <class 'tuple'> <class 'tuple'>
+    tuple1: tuple[int, int] = (1, 2)
+    tuple2: tuple[int, str, list[int]] = (1, "string", [1, 2, 3])
+    tuple3: tuple[str, ...] = ("a", "b", "c")  # any length acceptable
+    logger.info(type(tuple1))  # <class 'tuple'>
+    logger.info(type(tuple2))  # <class 'tuple'>
+    logger.info(type(tuple3))  # <class 'tuple'>
 
     # Dict
-    # Dict is used in python 3.6 to 3.8 instead of dict which is used in python 3.9 or more
-    dict1: Dict[str, int] = {"a": 1, "b": 2}
-    dict2: Dict[int, str] = {1: "a", 2: "b"}
-    dict3: Dict[str, Union[List[str], int]] = {"a": ["aaa", "bbb"], "b": 1}
-    print(type(dict1), type(dict2), type(dict3))
-    # <class 'dict'> <class 'dict'> <class 'dict'>
+    dict1: dict[str, int] = {"a": 1, "b": 2}
+    dict2: dict[int, str] = {1: "a", 2: "b"}
+    dict3: dict[str, Union[list[str], int]] = {"a": ["aaa", "bbb"], "b": 1}
+    logger.info(type(dict1))  # <class 'dict'>
+    logger.info(type(dict2))  # <class 'dict'>
+    logger.info(type(dict3))  # <class 'dict'>
 
 
 def add(x: int, y: int) -> int:
@@ -196,6 +198,7 @@ def add(x: int, y: int) -> int:
     int
         Result of integer addition
     """
+    logger.debug("add")
     return x + y
 
 
@@ -214,7 +217,7 @@ def main(name: str) -> int:
         length of greeting target name
     """
 
-    print(f"Hello {name}")
+    logger.info(f"Hello {name}")
     return len(name)
 
 
@@ -222,6 +225,6 @@ if __name__ == "__main__":
 
     name = "Python"
     name_length = main(name)
-    print(name_length)
+    logger.info(name_length)
     basic_typing_sample()
     advanced_typing_sample()
